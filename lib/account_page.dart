dbb54792c3fcabee240fc69e30a59fddc19220fb
Duaa Ahmed <ektfaaduaa@gmail.com>
2024-08-19 11:15:24 +0300

    first commit

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 
 class AccountPage extends StatelessWidget {
   AccountPage({super.key});
-  List<String> Categories = [
-    'Favorite',
-    'Edit Account',
-    'Settings and Privacy',
-    'Help'
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,34 +21,6 @@ class AccountPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Stack(
-              children: [
-                const CircleAvatar(
-                  radius: 40,
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: Container(
-                    padding: const EdgeInsets.all(2),
-                    decoration: BoxDecoration(
-                      color: const Color(0xff3D5CFF),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(Icons.camera_alt,
-                        color: Colors.white, size: 16),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 10,
-                  left: 10,
-                  top: 10,
-                  child: Image.asset('assets/avva.png'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
             Expanded(
               child: ListView.builder(
                 itemCount: Categories.length,
@@ -74,62 +40,6 @@ class AccountPage extends StatelessWidget {
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: 0,
-        backgroundColor: const Color(0xff1F1F39),
-        unselectedItemColor: const Color(0xff858597),
-        unselectedLabelStyle: const TextStyle(
-            fontFamily: 'poppins',
-            color: Color(0xff858597),
-            fontSize: 11,
-            fontWeight: FontWeight.w500),
-        selectedItemColor: Color(0xff858597),
-        selectedLabelStyle: const TextStyle(
-            fontFamily: 'poppins',
-            color: Color(0xff858597),
-            fontSize: 11,
-            fontWeight: FontWeight.w500),
-        iconSize: 24,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: Color(0xff858597),
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.book,
-              color: Color(0xff858597),
-            ),
-            label: 'Course',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-              size: 40,
-              color: Color(0xff858597),
-            ),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.message,
-              color: Color(0xff858597),
-            ),
-            label: 'Message',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: Color(0xff858597),
-            ),
-            label: 'Account',
-          ),
-        ],
       ),
     );
   }
